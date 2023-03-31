32a76586c844f63917c34c4217b03f35e8acc04e
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-03-31 20:37:02 +0700

    configuring mysql database

---
+++ server/migrations/20230331132921-create-product-detail.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ProductDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ProductId: {
+        type: Sequelize.INTEGER,
+        references: {model: {tableName: 'Products'}, key: 'id'}
+      },
+      range: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ProductDetails');
+  }
+};
